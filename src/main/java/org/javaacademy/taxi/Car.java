6be06882c849af283eed6cd70b386d0f635db5d8
Lex-Smith <e.d.smith@mail.ru>
2024-03-28 22:49:49 +0300

    refactored Car.jar and application-luna.yaml

--- src/main/java/org/javaacademy/taxi/Car.java
+++ src/main/java/org/javaacademy/taxi/Car.java
@@ -1,9 +1,7 @@
 package org.javaacademy.taxi;
 
 import java.math.BigDecimal;
-import java.util.Map;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Car {
@@ -15,33 +13,10 @@ public class Car {
     private String carNumber;
     @Getter
     private BigDecimal earnedMoney = BigDecimal.ZERO;
-//    private Map<String, Integer> tariffSchedule = Map.of(
-//            "Березовая роща", 10,
-//            "Кандикюля", 4,
-//            "Строитель", 12
-//    );
 
     public Car(String carNumber) {
         this.carNumber = carNumber;
     }
-
-    /*
-    public void takeOrderMap(Customer customer, boolean isDay) throws Exception {
-        BigDecimal orderCost;
-        BigDecimal rate = nightRate;
-        if (isDay) {
-            rate = dayRate;
-        }
-        if (tariffSchedule.containsKey(customer.getAddress())) {
-            orderCost = rate.multiply(BigDecimal
-                    .valueOf(tariffSchedule.get(customer.getAddress())));
-        } else {
-            throw new Exception("Адреса нет в базе");
-        }
-        earnedMoney = orderCost.divide(BigDecimal.valueOf(2), 2);
-    }
-
-     */
 
     public BigDecimal takeOrder(Customer customer, boolean isDay) throws Exception {
         int rate = nightRate;
